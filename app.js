7b47074dd8b9486f02a04b2085d04b40054d223c
Tejash Patel <tejashpatel3402@gmail.com>
2019-11-22 00:46:56 -0500

    build static version (no dynamic data)

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ var express = require('express');
 
 const app = express();
 app.set("view engine", "ejs");
+app.use('/static', express.static('public'));
+
 
 app.get("/", (req, res) => {
+    res.redirect("/updates");
+})
+
+app.get("/updates", (req, res) => {
     res.render("index.ejs");
+})
+
+app.get("/schedule", (req, res) => {
+    res.render("schedule.ejs");
 })
 
 app.listen(process.env.PORT, (req, res) => {
