e5ac53991c4d32c8948d73878c3d9838f305b99c
Tejash Patel <tejashpatel3402@gmail.com>
2019-11-25 15:41:28 -0500

    set app to use end points

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var request = require('request');
+var moment = require('moment');
 
 const app = express();
 app.set("view engine", "ejs");
@@ -17,14 +18,34 @@ app.get("/updates", (req, res) => {
             console.log(error);
         } else {
             var updates = JSON.parse(body);
-            res.render("index.ejs", { updates: updates });
+            var arr = [];
+            for (var id in updates.updates) {
+                updates.updates[id].deliveryTime = moment(updates.updates[id].deliveryTime).fromNow();
+                arr.push(updates.updates[id]);
+            }
+            res.render("index.ejs", { updates: arr });
 
         }
     });
 })
 
 app.get("/schedule", (req, res) => {
-    res.render("schedule.ejs");
+    request(baseUrl + "/schedule", function (error, response, body) {
+        if (error) {
+            console.log(error);
+        } else {
+            var schedule = JSON.parse(body);
+            var arr = [];
+            for (var id in schedule.events) {
+                schedule.events[id].startTime = moment(schedule.events[id].startTime).format("hh:mm A");
+                schedule.events[id].endTime = moment(schedule.events[id].endTime).format("hh:mm A");
+
+                arr.push(schedule.events[id]);
+            }
+            res.render("schedule.ejs", { schedule: arr });
+
+        }
+    });
 })
 
 app.get("/error", (req, res) => {
