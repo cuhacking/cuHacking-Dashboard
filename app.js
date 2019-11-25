1bdaa5358898bb9b3480bc4ab24cd8a19b6ddc56
Tejash Patel <tejashpatel3402@gmail.com>
2019-11-25 00:27:38 -0500

    get data from api

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 var express = require('express');
+var request = require('request');
 
 const app = express();
 app.set("view engine", "ejs");
 app.use('/static', express.static('public'));
 
+var baseUrl = "https://cuhacking.com/api-dev";
 
 app.get("/", (req, res) => {
     res.redirect("/updates");
 })
 
 app.get("/updates", (req, res) => {
-    res.render("index.ejs");
+    request(baseUrl + "/updates", function (error, response, body) {
+        if (error) {
+            console.log(error);
+        } else {
+            var updates = JSON.parse(body);
+            res.render("index.ejs", { updates: updates });
+
+        }
+    });
 })
 
 app.get("/schedule", (req, res) => {
     res.render("schedule.ejs");
+})
+
+app.get("/error", (req, res) => {
+    res.render("error.ejs");
 })
 
 app.listen(process.env.PORT, (req, res) => {
